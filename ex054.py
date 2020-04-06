16c3246948955a3df141f801d6a081adc8a9750a
Kewelly Batista <kewinha21@gmail.com>
2020-04-05 21:05:36 -0300

    ...

--- ex054.py
+++ ex054.py
@@ -2,12 +2,12 @@ from datetime import date
 maior = 0
 menor = 0
 data = date.today().year
-for pessoa in range (1, 8):
+for pessoa in range(1, 8):
     nas = int(input(f'Dígite o ano de nascimento da {pessoa}° pessoa: '))
     idade = data - nas
     if idade < 18:
         menor += 1
     elif idade >= 18:
-        maior +=1
+        maior += 1
 print(f'{menor} são menores de idade,')
 print(f'{maior} são maiores de idade')
